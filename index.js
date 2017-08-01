7c5ce15e985e759a0d03792eddb6fac3a6dea1c9
Nolonwabo <nolonwabomahanjana13@gmail.com>
2017-08-01 15:13:44 +0200

    Added a Procfile.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 "use strict";
+var countNamesGreeted = 0;
 var express = require('express');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
@@ -15,74 +16,27 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json())
 
 app.get('/greetings', function(req, res) {
-   var name = req.body.name;
+  var name = req.body.name;
   res.render('index');
 });
-
-// how to greet a user
-//function createMessage(language, name){
-
-//}
-
-// how many times a user has been greeted
-//function counterForName(name){
-
-//}
-
+var count = 0;
 app.post('/greetings', function(req, res) {
   var name = req.body.name;
   var language = req.body.language;
-
-  // the counter
-//  const counter = counterForName(name);
-
-  // the Message
-  //const message = createMessage(language, name);
-
-//  res.render('index' , {
-  //  counter : counter,
-   //message : message
-//  });
-
-   if (language === 'english') {
-  res.render('index', {
-       greet: 'Hello, ' + name
-    });
-
+  var message = '';
+  if (language === 'english') {
+    message = 'Hello , ' + name;
+    count++;
   } else if (language === 'afrikaans') {
-     res.render('index', {
-       greet: 'Hallo, ' + name
-   })
+    message = 'Hallo , ' + name;
+    count++;
   } else if (language === 'isixhosa') {
-    res.render('index', {
-       greet: 'Molo, ' + name
-     })
-   }
+    message = 'Hello , ' + name;
+    count++;
+  }
 
+  res.render('index', {
+    greet: message,
+    number: count
+  })
 });
-
-
-
-
-//var greeted =[];
-//app.get('/greetings/:name', function(req, res){
-//res.send('Hello, ' +req.params.name);
-//  greeted.push(req.params.name);
-//  console.log(req.params.name);
-//});
-//  app.get('/greeted', function(req, res){
-//  res.send(greeted);
-//console.log(greeted);
-//});
-//app.get('/counter/:user_name', function(req, res){
-//var user = req.params.user_name;
-//var counterMap ={}
-//for(i=0; i<greeted.length; i++){
-//var loop = greeted[i];
-//if (user===loop) {
-//  counterMap[loop]=counterMap[loop] ? counterMap[loop]+1:1;
-//  }
-//}
-//res.send('Hello, ' + user + ' has been greeted ' + counterMap[user] +' time(s)');
-
-//})
