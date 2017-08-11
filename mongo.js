e3196dc4d888278efbb9e60302ea1772c2812023
Nolonwabo <nolonwabomahanjana13@gmail.com>
2017-08-11 15:25:55 +0200

    final commit

--- mongo.js
+++ mongo.js
@@ -1,8 +1,6 @@
 var mongoose = require('mongoose');
 const mongoURL = process.env.MONGO_DB_URL || "mongodb://nolo:nolo@ds117093.mlab.com:17093/web-applicaction"
-mongoose.connect('mongoURL', {
-  useMongoClient: true
-});
+mongoose.connect(mongoURL);
 
 exports.storeName = mongoose.model('storeName', {
  count: Number,
