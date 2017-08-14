16cd63df55e1a94664c6fdb0c7e27b925475b162
Nolonwabo <nolonwabomahanjana13@gmail.com>
2017-08-14 10:38:50 +0200

    connected to mongodb

--- mongo.js
+++ mongo.js
@@ -1,13 +1,8 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/index', {
+const mongoURL = "mongodb://nolo:nolo@ds117093.mlab.com:17093/web-applicaction";
+mongoose.connect('mongoURL', {
   useMongoClient: true
 });
-
-const mongoURL = process.env.MONGO_DB_URL || "'mongodb://localhost/index'";
-
-mongoose.connect(mongoURL);
-
-
 exports.storeName = mongoose.model('storeName', {
   count: Number,
   name: String
